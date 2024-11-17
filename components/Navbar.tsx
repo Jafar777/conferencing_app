90e1fc684e1ab19684790b4436a8de004f3a3cd3
jafar <144897803+Jafar777@users.noreply.github.com>
2024-11-17 14:41:04 +0300

    shadcdn addition

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href='/' className='flex items-center gap-1'>
+        <Image 
+          src='/icons/logo.svg'
+          width={32}
+          height={32}
+          alt='yoom'
+          className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+      </Link>
+      <div className='flex-between gap-5 m:size-10'>
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
