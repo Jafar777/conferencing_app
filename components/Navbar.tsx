0b1960fad80bd002859133e4df686d434dc819f5
jafar <144897803+Jafar777@users.noreply.github.com>
2024-11-21 11:18:50 +0300

    clerk-addition

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -14,9 +15,12 @@ const Navbar = () => {
           alt='yoom'
           className='max-sm:size-10'
         />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Omayyah</p>
       </Link>
       <div className='flex-between gap-5 m:size-10'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
